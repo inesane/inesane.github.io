265382f7977338d6e9eba42116922212a62efe66
inesane <sannidhiainu@gmail.com>
2026-03-07 15:39:37 +0530

    Added portfolio tracker

--- js/theme.js
+++ js/theme.js
@@ -66,7 +66,8 @@ let project_descriptions = {
     "website": `Code for this website! <br><br>Coded based on a colorlib template and a combination of multiple codepens and hosted using GitHub pages.`,
     "tcav": `Built on TCAV, an ML paper that aims to find a link between recognizing an object and how important a certain feature is to that objects detection. <br><br>Implemented baseline TCAV model and attempted to extend this idea in multiple manners including detecting medical conditions such as arthritis using the presence of bone spurs.`,
     "raytracing": `Implemented a ray tracing renderer capable of simulating realistic lighting and material interactions in 3D scenes. <br><br>Rendered diffuse, reflective and transparent objects and optimized rendering using bounding volume hierarchies (BVH).`,
-    "workout": `Data merger for workout app data (Lyfta, Strong, etc) and Garmin heart-rate activity files. <br><br>Provides set-level insights like HR curves, averages, peaks, and a complete workout timeline.`
+    "workout": `Data merger for workout app data (Lyfta, Strong, etc) and Garmin heart-rate activity files. <br><br>Provides set-level insights like HR curves, averages, peaks, and a complete workout timeline.`,
+    "investment": `A local, self-hosted investment portfolio tracker. <br><br>Track stocks, ETFs, gold ETFs, mutual funds, crypto, PF, FDs with live prices and historical charts.`
 }
 
 
